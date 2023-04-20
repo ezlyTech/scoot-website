ccb885fc7c37020eb8408c5250db3e385cef1173
jmagwili <joshuamagwili@gmail.com>
2023-04-20 21:02:20 +0800

    made code readable, minor changes

--- js/header.js
+++ js/header.js
@@ -30,11 +30,4 @@ function navMenu() {
   
 document.querySelector('.header__icon')
         .addEventListener('click', navMenu);
-
-$(".Click-here").on('click', function() {
-  $(".custom-model-main").addClass('model-open');
-}); 
-$(".close-btn, .bg-overlay").click(function(){
-  $(".custom-model-main").removeClass('model-open');
-});
-
+        
